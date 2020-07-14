08fe4bdecafffc4cfe0ae3db8b7e529940d570ff
tbf <contact@trailblazingfive.net>
2020-07-14 07:08:19 +0200

    new album change

---
+++ src/components/elements/LargeAlbum.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `useStaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+const Container = styled.div`
+  width: 500px;
+  margin: 10px;
+  background: #eef5fb;
+  padding: 10px;
+  border-radius: 5px;
+  &:hover{
+    background: #ed98db;
+    padding: 15px;
+    margin: 5px;
+  }
+`
+
+const LargeAlbum = (props) => {
+  const data = useStaticQuery(graphql`
+  query {
+    allFile(filter: {relativeDirectory: {eq: "albums"}, name: {}}) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 900) {
+              originalName
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`)
+
+
+  let cover = data.allFile.edges.filter(allCovers => allCovers.node.childImageSharp.fluid.originalName === props.albumCover)[0]
+
+
+  return (
+    <Container>
+      <a href={props.link} target="_blank" rel="noopener noreferrer" title={props.title}>
+        <Img fluid={cover.node.childImageSharp.fluid} />
+      </a>
+    </Container>
+  )
+}
+
+export default LargeAlbum
